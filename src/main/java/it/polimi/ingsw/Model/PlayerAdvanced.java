bda0b9c8e0dbe2708c252a346c493ed96b25e9de
GiadaSilvestrini <giada.silvestrini@mail.polimi.it>
2022-03-15 11:48:07 +0100

    some model classes added

---
+++ src/main/java/it/polimi/ingsw/Model/PlayerAdvanced.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.Model;
+
+public class PlayerAdvanced extends Player{
+    private Player p;
+    private Coin c;
+    public PlayerAdvanced(Player p2){
+        p=p2;
+    }
+    public void addCoin(){
+
+    }
+}
