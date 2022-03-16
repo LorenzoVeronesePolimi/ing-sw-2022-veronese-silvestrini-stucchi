ac7fea05bb67fa084e4a3993c51fd9bbd3ea9df4
GiadaSilvestrini <giada.silvestrini@mail.polimi.it>
2022-03-16 19:22:02 +0100

    Board interface modified

--- src/main/java/it/polimi/ingsw/Model/Board/Board.java
+++ src/main/java/it/polimi/ingsw/Model/Board/Board.java
@@ -3,37 +3,31 @@ package it.polimi.ingsw.Model.Board;
 import it.polimi.ingsw.Model.Bag;
 import it.polimi.ingsw.Model.Enumerations.SPColour;
 import it.polimi.ingsw.Model.Pawns.MotherNature;
+import it.polimi.ingsw.Model.Pawns.Student;
 import it.polimi.ingsw.Model.Places.Archipelago;
 import it.polimi.ingsw.Model.Places.Cloud;
 import it.polimi.ingsw.Model.Places.School;
 import it.polimi.ingsw.Model.Player;
 
+import java.util.List;
+import java.util.Map;
+
 public interface Board {
-    private School schools[];
-    private Archipelago archipelago[];
-    private Cloud clouds[];
-    private MotherNature mn;
-    private Bag bag;
+    List<School> schools=null;
+    List<Archipelago> archipelago=null;
+    List<Cloud> clouds=null;
+    MotherNature mn=null;
+    Bag bag=null;
 
-    public void Board(int numberPlayers){
-
-    }
-    public int getMotherNaturePosition(){
-
-    }
-    public void moveStudentSchoolToCloud(Player p1, SPColour c, Cloud cl){
-
-    }
-    public void moveStudentSchoolToArchipelagus(Player p1, SPColour c, Cloud cl, Archipelago a){
-
-    }
-    public void moveMotherNature(int numPos){
-
-    }
-    public void moveProfessor(SPColour c, Player dest){
-
-    }
-    public School whereIsProfessor(SPColour c){
-
-    }
+    Map<Player, School> playerSchool=null;
+    void Board(List<Player> players);
+    void moveStudentSchoolToArchipelagos(Player p1, SPColour c, Archipelago a);
+    void moveStudentCloudToSchool(Player p1, Cloud cloud);
+    void moveStudentHallToDiningRoom(Player p1, SPColour c);
+    void moveStudentBagToCloud(Cloud cloud);
+    void moveStudentBagToSchool(Player p1);
+    int getMotherNaturePosition();
+    void moveMotherNature(int numPos);
+    void moveProfessor(SPColour c, Player dest);
+    School whereIsProfessor(SPColour c);
 }
--- src/main/java/it/polimi/ingsw/Model/Game/GameAdvanced.java
+++
@@ -1,9 +0,0 @@
-package it.polimi.ingsw.Model.Game;
-
-import it.polimi.ingsw.Model.Cards.CharacterCard;
-
-public interface GameAdvanced {
-    public void getRandomCharCards();
-
-    public void activateEffect(CharacterCard card);
-}
