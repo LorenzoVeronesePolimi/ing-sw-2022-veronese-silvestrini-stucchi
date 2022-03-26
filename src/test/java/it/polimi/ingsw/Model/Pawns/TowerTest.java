a7aa5dba69ae0301442c5beadfb1a2f7ea2b1e51
William Stucchi <william.stucchi@mail.polimi.it>
2022-03-26 14:55:32 +0100

    Tests in Tower

--- src/test/java/it/polimi/ingsw/Model/Pawns/TowerTest.java
+++ src/test/java/it/polimi/ingsw/Model/Pawns/TowerTest.java
@@ -1,4 +1,31 @@
 package it.polimi.ingsw.Model.Pawns;
 
+import it.polimi.ingsw.Model.Enumerations.PlayerColour;
+import it.polimi.ingsw.Model.Player;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.*;
+
 public class TowerTest {
+    Tower testedTower = null;
+
+    @Test
+    void getPlayer() {
+        Player player = new Player("nickname", PlayerColour.BLACK);
+
+        testedTower = new Tower(player);
+
+        assertEquals(player, testedTower.getPlayer());
+    }
+
+    @Test
+    void toStringTest() {
+        Player player = new Player("nickname", PlayerColour.BLACK);
+
+        testedTower = new Tower(player);
+
+        assertEquals("Tower{" +
+                "player=" + testedTower.getPlayer() +
+                '}', testedTower.toString());
+    }
 }
