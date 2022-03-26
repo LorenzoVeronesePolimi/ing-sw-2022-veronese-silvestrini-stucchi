e06e0ef8bd0e395004f040e4d336842fad9b111c
GiadaSilvestrini <giada.silvestrini@mail.polimi.it>
2022-03-26 17:20:12 +0100

    MotherNatureTest.java implemented

--- src/test/java/it/polimi/ingsw/Model/Pawns/MotherNatureTest.java
+++ src/test/java/it/polimi/ingsw/Model/Pawns/MotherNatureTest.java
@@ -1,4 +1,16 @@
 package it.polimi.ingsw.Model.Pawns;
 
+import it.polimi.ingsw.Model.Places.Archipelago;
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
+
+
 public class MotherNatureTest {
+    @Test
+    void putInPosition(){
+        MotherNature mn = new MotherNature();
+        Archipelago dest = new Archipelago();
+        mn.putInPosition(dest);
+        assertEquals(dest, mn.getCurrentPosition());
+    }
 }
