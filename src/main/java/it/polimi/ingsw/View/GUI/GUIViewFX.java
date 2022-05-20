9f8738a745706e8292811f19cafb8fcff70be40b
LorenzoVeronese <lorenzo1.veronese@mail.polimi.it>
2022-05-20 10:30:47 +0200

    Try with javaFX

--- src/main/java/it/polimi/ingsw/View/GUI/GUIViewFX.java
+++ src/main/java/it/polimi/ingsw/View/GUI/GUIViewFX.java
@@ -1,14 +1,24 @@
 package it.polimi.ingsw.View.GUI;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class GUIViewFX extends Application {
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws IOException {
+        /*
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/try.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();*/
+
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
         Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
