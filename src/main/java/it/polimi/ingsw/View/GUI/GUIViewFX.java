2d474ce0a11de0f8bb0c16fa9aba5e0c03ebae7f
LorenzoVeronese <lorenzo1.veronese@mail.polimi.it>
2022-05-20 14:53:15 +0200

    corrected fxml reading

--- src/main/java/it/polimi/ingsw/View/GUI/GUIViewFX.java
+++ src/main/java/it/polimi/ingsw/View/GUI/GUIViewFX.java
@@ -13,18 +13,18 @@ import java.io.IOException;
 public class GUIViewFX extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        /*
+
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/try.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
-        stage.show();*/
-
+        stage.show();
+        /*
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
         Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
         Scene scene = new Scene(new StackPane(l), 640, 480);
         stage.setScene(scene);
-        stage.show();
+        stage.show();*/
     }
 
     public static void main(String[] args) {
